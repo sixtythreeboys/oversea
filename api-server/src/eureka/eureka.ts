f2de8bde0fa82a11e9cce45c94621960aa031082
Lee Han Seong <jkhk7788@gmail.com>
2023-08-01 18:46:27 +0900

    u

--- api-server/src/eureka/eureka.ts
+++ api-server/src/eureka/eureka.ts
@@ -2,16 +2,22 @@ import { Eureka } from 'eureka-js-client';
 import config from '../../config';
 
 export function apply() {
-  const client = new Eureka(config.EUREKA);
-  try {
-    client.start();
-  } catch (e) {
-    console.log(e);
+  async function start() {
+    try {
+      client.start();
+    } catch (e) {
+      console.log(e);
+    }
   }
+  const client = new Eureka(config.EUREKA);
+  client.on('deregistered', (e) => {
+    start();
+  });
   process.on('SIGINT', () => {
     client.stop(() => {
       console.log('Node.js app unregistered from Eureka server');
       process.exit();
     });
   });
+  start();
 }
